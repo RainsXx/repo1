978ffacdd56a2a7cd46d01785c5d3aaa6c387c4e
admin <952798234@qq.com>
2019-04-11 22:06:23 +0800

    修改heelo

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,9 +1,21 @@
 import org.junit.Test;
 
+import javax.sound.midi.Soundbank;
+
 public class Main {
 
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+    }
+
+    public void test() {
+        int i = 0;
+        i += 10;
+        System.out.println(i);
     }
 }
